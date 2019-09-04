dd34f4893ba7a6fa647ca9a6a548f689279a0bb1
Matheus Kindrazki <matheus@mentores.com.br>
2019-09-03 22:28:38 -0300

    buscando dados e issues github

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository() {
-  return <h1>Repositórios</h1>;
+export default class Repository extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repository: {},
+      issues: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      loading: false,
+      repository: repository.data,
+      issues: issues.data,
+    });
+  }
+
+  render() {
+    const { loading, repository, issues } = this.state;
+    return <h1 />;
+  }
 }
